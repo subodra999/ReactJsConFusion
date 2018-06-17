c4fc28644f695751438f654278081bdc7e1eb012
subodra <subodra999@gmail.com>
2018-06-17 17:40:06 +0530

    assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -52,6 +53,7 @@ class Main extends Component {
                 <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                 <Route exact path='/contactus' component={Contact} />
                 <Route path='/menu/:dishId' component={DishWithId} />
+                <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                 <Redirect to='/home' />
             </Switch>
             <Footer />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { K } from 'handlebars';
 
 function RenderMenuItem({ dish }) {
 
