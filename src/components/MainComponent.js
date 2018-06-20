85954b325badab0b0d12929b9e6fdff715d6d433
subodra <ext-subodra.banik@medlife.com>
2018-06-20 16:12:50 +0530

    build and done

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -34,9 +34,6 @@ const mapDispatchToProps = dispatch => {
 
 class Main extends Component {
 
-    constructor(props) {
-        super(props);
-    }
 
     componentDidMount() {
         this.props.fetchDishes();
